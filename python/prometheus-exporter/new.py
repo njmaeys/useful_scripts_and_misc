1d58b7b23c4104ce106350acd1882a16a4b95d41
njmaeys <nmaeys@hotmail.com>
2021-05-27 11:13:38 -0500

    whoops

--- python/prometheus-exporter/new.py
+++ python/prometheus-exporter/new.py
@@ -3,35 +3,36 @@ import random
 import time
 from prometheus_client import start_http_server, Gauge
 
-MY_GAUGE = Gauge('custom_process_name', 'Check status of service', ['host', 'process'])
+SNOW_GAUGE = Gauge('onspring_to_service_now_py', 'Check status of service', ['host', 'process'])
 
 def failures():
   randone = random.randint(0, 10)
   randtwo = random.randint(0, 20)
   return [
     {
-      'process': 'process_one',
+      'process': 'failed_new_cases',
       'val': randone,
     },
     {
-      'process': 'process_two',
+      'process': 'failed_existing_cases',
       'val': randtwo,
     },
   ]
 
-def generate(failures, ge):
+def generate_prom_metrics(host, failures, ge):
   for f in failures:
     ge.labels(host, f['process']).set(f['val'])
+  return
 
 
 if __name__ == '__main__':
   start_http_server(8000)
   while True:
-    ge = MY_GAUGE
+    ge = SNOW_GAUGE
     host = os.uname()[1]
 
     fails = failures()
 
-    generate(fails, ge)
+    generate_prom_metrics(host, fails, ge)
 
     time.sleep(15)
